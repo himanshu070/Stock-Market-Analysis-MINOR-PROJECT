19094b683f4bd037f5294b9c82742354985a80d7
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2023-04-25 03:18:32 +0530

    Head title

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import {HiInformationCircle} from "react-icons/hi"
 import { useEffect, useRef, useState } from 'react'
 import Document from './components/Document'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
@@ -63,68 +64,73 @@ export default function Home() {
   
 
   return (
-    <main className="main-div">
-      <div className="header flex flex-col items-center justify-center flex-wrap">
-        {" "}
-        <div className="text-6xl font-extrabold m-2">
-          Stock Market Price Prediction
-        </div>
-        <div className="text-lg border-b-2 border-slate-600 description text-slate-300 p-2">
-          Price prediction of different companies trained and tested using
-          linear regression
-        </div>
-      </div>
-
-      <div className="buttons flex flex-wrap items-center justify-around mx-4">
-        <div className="font-size w-80 m-2 flex flex-col justify-center items-center">
-          <label>Companies:</label>
-          <select
-            id="company"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={companyName}
-            onChange={handleCompany}
-            ref={compRef}
-          >
-            <option value="tcs">TCS</option>
-            <option value="sbin">SBIN</option>
-            <option value="britania">Britannia</option>
-            <option value="adani">Adani</option>
-          </select>
-        </div>
-        <div className="text-4xl cursor-pointer hover:text-blue-200">
-          <HiInformationCircle onClick={() => setIsDocument(!isDocument)} />
-        </div>
-        <div className="font-size w-80 m-2 flex flex-col justify-center items-center">
-          <label>Graph:</label>
-          <select
-            id="company"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value={graphName}
-            onChange={handleGraph}
-            ref={graphRef}
-          >
-            <option value="heatmap">Heatmap</option>
-            <option value="boxplot">Box Plot</option>
-            <option value="histogram">Histogram</option>
-            <option value="scatter">Scatter Plot</option>
-            <option value="predictedvsactual">
-              Predicted vs Actual Values
-            </option>
-          </select>
-        </div>
-      </div>
-
-      <div className="content w-screen flex items-center justify-center">
-        {isDocument ? (
-          <div className="documents overflow-y-scroll text-bg text-area">
-            <p className="text-gray-700"><Document/></p>
+    <>
+      <Head><title>Stock Prediction</title></Head>
+      <main className="main-div">
+        <div className="header flex flex-col items-center justify-center flex-wrap">
+          {" "}
+          <div className="text-6xl font-extrabold m-2">
+            Stock Market Price Prediction
           </div>
-        ) : (
-          <div className="images m-10">
-            <Image src={showImg} width={900} height={900} />
+          <div className="text-lg border-b-2 border-slate-600 description text-slate-300 p-2">
+            Price prediction of different companies trained and tested using
+            linear regression
           </div>
-        )}
-      </div>
-    </main>
+        </div>
+
+        <div className="buttons flex flex-wrap items-center justify-around mx-4">
+          <div className="font-size w-80 m-2 flex flex-col justify-center items-center">
+            <label>Companies:</label>
+            <select
+              id="company"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={companyName}
+              onChange={handleCompany}
+              ref={compRef}
+            >
+              <option value="tcs">TCS</option>
+              <option value="sbin">SBIN</option>
+              <option value="britania">Britannia</option>
+              <option value="adani">Adani</option>
+            </select>
+          </div>
+          <div className="text-4xl cursor-pointer hover:text-blue-200">
+            <HiInformationCircle onClick={() => setIsDocument(!isDocument)} />
+          </div>
+          <div className="font-size w-80 m-2 flex flex-col justify-center items-center">
+            <label>Graph:</label>
+            <select
+              id="company"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={graphName}
+              onChange={handleGraph}
+              ref={graphRef}
+            >
+              <option value="heatmap">Heatmap</option>
+              <option value="boxplot">Box Plot</option>
+              <option value="histogram">Histogram</option>
+              <option value="scatter">Scatter Plot</option>
+              <option value="predictedvsactual">
+                Predicted vs Actual Values
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <div className="content w-screen flex items-center justify-center">
+          {isDocument ? (
+            <div className="documents overflow-y-scroll text-bg text-area">
+              <p className="text-gray-700">
+                <Document />
+              </p>
+            </div>
+          ) : (
+            <div className="images m-10">
+              <Image src={showImg} width={900} height={900} />
+            </div>
+          )}
+        </div>
+      </main>
+    </>
   );
 }
